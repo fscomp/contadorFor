bd7cbf2f47ed62a075714fdbd426403aa91c46f1
fscomp <32854253+fscomp@users.noreply.github.com>
2017-10-30 17:51:24 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using std::cout;
+int main(void)
+{
+  int contador; 
+  for(contador = 1; contador <= 10; contador++)
+  {
+      cout << contador;
+  }
+  
+  return(0);
+}
